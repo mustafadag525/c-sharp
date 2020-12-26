164341372bf1b459e6a0805d5c0ca122128115ac
mustafadag525 <55752233+mustafadag525@users.noreply.github.com>
2020-12-26 17:19:06 +0300

    Add files via upload

---
+++ oop tckimlik kontrol/Musteri.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_3
+{
+   internal class Musteri
+    {
+
+        private string tckimliknumarası;
+        public string Tckimlikno
+        {
+            get
+            {
+                return tckimliknumarası.Substring(0,3);
+            }
+            set
+            {
+                if (value.Length == 11)
+                {
+                    bool kontrol = false;
+                    for (int i = 0; i < value.Length; i++)
+                    {
+                        bool karekterkontrol = char.IsNumber(value[i]);
+                        if (karekterkontrol)
+                        {
+                            //
+                        }
+                        else
+                        {
+                            kontrol = true;
+                            break;
+                        }
+                    }
+                    if (kontrol)
+                    {
+                        Console.WriteLine("tc kimlik içindeki değerler sayısal olmalıdır!!!");
+                        
+                    }
+                    else
+                    {
+                        this.tckimliknumarası = value;
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("tc kimlik numarası 11 hane olmalıdır");
+                     
+                }
+            }
+
+
+        }
+
+
+
+
+
+
+    }
+}
---
+++ oop tckimlik kontrol/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri M1 = new Musteri();
+         Console.WriteLine("tc kimlik numarası gir: ");
+            M1.Tckimlikno = Console.ReadLine();
+            Console.WriteLine(M1.Tckimlikno);
+            Console.ReadLine();
+            
+            
+        }
+    }
+}
