d8371eaaa69c11294812a70a53ccda5e19b535df
mustafadag525 <55752233+mustafadag525@users.noreply.github.com>
2020-12-26 17:14:05 +0300

    Add files via upload

---
+++ OgrenciBilgi/OgrenciBilgi/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OgrenciBilgi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ögrenci ogr = new Ögrenci();
+            ogr.isim = "mustafa";
+            ogr.soyisim = "dağ";
+            ogr.tckimlik = "1234567890";
+            ogr.cinsiyet = "erkek";
+            ogr.Emailadres = "MSTAQE124";
+            ogr.Adres = "green park a blok 6 numara";
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OgrenciBilgi/OgrenciBilgi/Ögrenci.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OgrenciBilgi
+{
+   public class Ögrenci
+    {
+        public static string domainadres = "dag.com";
+        public string isim { get; set; }
+        public string soyisim { get; set; }
+        public string tckimlik { get; set; }
+        public string cinsiyet { get; set; }
+
+
+        private string emailadres;
+        public string Emailadres
+        {
+
+            get
+            {
+                return this.emailadres;
+            }
+            set
+            {
+                this.emailadres = value.ToLower() + "@" + Ögrenci.domainadres;
+            }
+
+        }
+        private string adres;
+        public string Adres {
+
+
+            get
+            {
+                return this.adres;
+            }
+            set
+            {
+                this.adres = value + " ŞANLIURFA /HALİLİYE ";
+            }
+        }
+    }
+}
