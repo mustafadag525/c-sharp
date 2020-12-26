6debfc572987f12d5ef48570453030bb56d46f6a
mustafadag525 <55752233+mustafadag525@users.noreply.github.com>
2020-12-26 17:17:10 +0300

    Add files via upload

---
+++ oop not hesap/Matematik.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ornek1._1
+{
+  public  class Matematik
+  {
+       
+     public void nothesapla(string kullanıcıadı,string kullanıcısoyad,decimal n1,decimal n2,decimal n3)
+        {
+            Console.WriteLine("ad--{0},soyad---{1} ", kullanıcıadı, kullanıcısoyad);
+            decimal ortalama = (n1 + n2 + n3) / 3;
+            if(ortalama<=45)
+            {
+                Console.WriteLine("ortalama={0}, kaldınız", ortalama);
+            }
+            else
+            {
+                Console.WriteLine("ortalama={0}, geçtiniz", ortalama);
+            }
+        }
+
+
+    }
+}
---
+++ oop not hesap/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ornek1._1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string ad = string.Empty;
+            string soyad = string.Empty;
+            decimal not1 = 0, not2 = 0, not3 = 0;
+            decimal ortalam = 0;
+            Console.WriteLine("kullanıcı adı: ");
+            ad = Console.ReadLine();
+            Console.WriteLine("kullanıcı soyadı: ");
+            soyad = Console.ReadLine();
+
+            Console.WriteLine("not1: ");
+            not1 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("not2: ");
+            not2 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("not3: ");
+            not3 = Convert.ToDecimal(Console.ReadLine());
+
+
+            Matematik M = new Matematik();
+            M.nothesapla( ad,  soyad,  not1,  not2,  not3);
+
+
+            Console.ReadLine();
+        }
+    }
+}
