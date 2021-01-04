569655ec5ff18cb26ac0def3fd41e57bd4d19865
mustafadag525 <55752233+mustafadag525@users.noreply.github.com>
2021-01-04 15:56:37 +0300

    Add files via upload

---
+++ faktoriyel sınıf metodu kullanımı/Faktor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metot
+{
+    class Faktor
+    {
+
+        public void faktora(int sayi)
+        {
+            int faktor = 1;
+            for(int i=1;i<=sayi;i++)
+            {
+                faktor *= i;
+            }
+            Console.WriteLine("faktor: " + faktor);
+        }
+
+    }
+}
---
+++ faktoriyel sınıf metodu kullanımı/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metot
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            int sayi;
+            Console.WriteLine("sayi gir: ");
+            sayi = Convert.ToInt32(Console.ReadLine());
+            Faktor a1 = new Faktor();
+            a1.faktora(sayi);
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
