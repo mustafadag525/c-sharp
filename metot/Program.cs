6a14da6701efa9a64f236e40fdea67e5a1403187
mustafadag525 <55752233+mustafadag525@users.noreply.github.com>
2021-01-31 15:56:59 +0300

    Add files via upload

---
+++ metot/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Topla t1 = new Topla();
+            int s1, s2;
+            Console.WriteLine("sayi gir:");
+            s1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("sayi gir:");
+            s2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("topla: " + t1.topla(s1, s2));
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ metot/Topla.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metot
+{
+public class Topla
+    {
+        public int topla(int sayi1,int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+        
+    }
+}
