30cd0edf4f5c2a0f6945de5d209bb8ea45b6147d
mustafadag525 <55752233+mustafadag525@users.noreply.github.com>
2021-02-01 23:30:12 +0300

    Add files via upload

---
+++ class metot-- ebob bulma/EbobBulma.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metot
+{
+   public class EbobBulma
+    {
+        public int ebobbul(int sayi1, int sayi2)
+        {
+            int ebob = 1;
+            for(int i=1;i<=sayi1 && i<=sayi2;i++)
+            {
+                if(sayi1%i==0 && sayi2%i==0)
+                {
+                    ebob = i;
+                }
+            }
+            return ebob;
+        }
+
+    }
+}
---
+++ class metot-- ebob bulma/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metot
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            EbobBulma E1 = new EbobBulma();
+            int s1, s2;
+            Console.WriteLine("sayi gir: ");
+            s1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("sayi gir: ");
+            s2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("ebob: " + E1.ebobbul(s1, s2));
+            Console.ReadLine();
+            
+        }
+    }
+}
